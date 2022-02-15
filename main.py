5e1845ee934f8ca0b87979e58fdb0cd47a550d99
Marcel P <marcel.pludra@t-online.de>
2022-02-15 19:22:20 +0000

    Get Server from description File

--- main.py
+++ main.py
@@ -5,28 +5,56 @@ T-REST: REST API Testing Tool by @MarcelPludra
 import sys
 
 from myparser import Parser
+import requests
+import logging
+import http
+
+# Set up logging to a file
+logging.basicConfig(filename="app.log", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+# Turn on global debugging for the HTTPConnection class, doing so will
+# cause debug messages to be printed to STDOUT
+http.client.HTTPConnection.debuglevel = 0
+
+
+
 
 params = sys.argv[1:]  # Parameter in params aufnehmen
 #OpenAPI description File
-OPENAPIFILE = "/Users/mpludra/OneDrive/02_Arbeit/Python-Kurs/eigenerOrdner/python/openapi.json"
+OPENAPIFILE = "/Users/mpludra/OneDrive/02_Arbeit/Python-Kurs/eigenerOrdner/specification.json"
+
+SERVER="http://petstore.swagger.io/api/v2/"
 
 while params:
     if params[0] == "-d":
         params.pop(0)
-        openapifile = params.pop(0)
+        OPENAPIFILE = params.pop(0)
         print()
         continue
 
-    # if params[0] == "-s":
-    #     params.pop(0)
-    #     sleep = int(params.pop(0))
-    #     continue
+    if params[0] == "-s":
+        params.pop(0)
+        SERVER= params.pop(0)
+        continue
 
     break
-
 
 
 if __name__ == '__main__':
 
     p=Parser(OPENAPIFILE)
-    print(p.get_all_paths())
+    #print(p.get_all_paths())
+    if p.get_servers() is not False:
+        SERVER=p.get_servers()
+        print(SERVER)
+
+    url=SERVER[0]+"/api/projects"
+    r = requests.get(url)
+    print(r.status_code)
+    print(r.json())
+
+
+
+
+
--- myparser.py
+++ myparser.py
@@ -6,6 +6,7 @@ class Parser:
     def __init__(self, file):
         self.__file = file
         self.__paths = {}
+        self.__servers=[]
         self.parse_file()
 
     def __str__(self):
@@ -24,3 +25,22 @@ class Parser:
 
         self.__paths = paths
         return self.__paths
+
+    def get_path(self,path):
+        return None
+
+    def get_servers(self):
+        self.__servers=[]
+        try:
+            servers=self.__data.get("servers")
+            for url in servers:
+                self.__servers.append(url.get("url"))
+            return self.__servers
+        except:
+            #Muss Log Nachricht ausgeben
+            print("No Server in description file")
+            return False
+
+
+
+
